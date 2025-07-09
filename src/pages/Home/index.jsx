da6a959edcf8d244f7fa91cbd40fd2bbefd55f1e
parsa501 <safazadehparsa@gmail.com>
2025-07-09 16:56:48 -0500

    add companise and componenets

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./Hero";
+import Companies from "./Companies";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center space-y-7 text-center">
       <Hero />
+      <Companies/>
     </div>
   );
 }
