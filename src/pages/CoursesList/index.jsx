7ba13fe89ae668c470199d66fc0d0f5d100bff9e
parsa501 <safazadehparsa@gmail.com>
2025-07-09 23:04:38 -0500

    add CoursesDetails

--- src/pages/CoursesList/index.jsx
+++ src/pages/CoursesList/index.jsx
@@ -2,7 +2,6 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import SearchBar from './SearchBar'
 import { AppContext } from '../../context/AppContext'
-
 import CourseCard from './CourseCard'
 import { assets } from '../../assets/assets'
 
@@ -12,20 +11,21 @@ export default function CoursesList() {
   const [filteredCourse, setFilteredCourse] = useState([])
 
   useEffect(()=>{
-    if(allCourses && allCourses.length >0){
+    if(allCourses && allCourses?.length >0){
       const tempCourses = allCourses.slice()
 
       input ? 
-       setFilteredCourse(tempCourses.filter(item => item.courseTitle.toLowerCase().includes(input.toLowerCase())))
+       setFilteredCourse(tempCourses.filter(item => item?.courseTitle?.toLowerCase()?.includes(input?.toLowerCase())))
        : setFilteredCourse(tempCourses)
     }
   }, [allCourses, input])
+  const items=filteredCourse?.map((course, index) => <CourseCard key={index} course={course}/>)
  return (
     <>
     <div className='relative md:px-36 px-8 pt-20 text-left'>
       <div className='flex md:flex-row flex-col gap-6 items-start justify-between w-full'>
         <div>
-        <h1 className='text-4xl font-semibold text-gray-800'>Course List</h1>
+        <h2 className='text-4xl font-semibold text-gray-800'>Course List</h2>
         <p className='text-gray-500'>
           <span className='text-blue-600 cursor-pointer' onClick={()=>navigate("/")}>Home</span>/
           <span>Course List</span>
@@ -36,15 +36,13 @@ export default function CoursesList() {
       {
         input && <div className='inline-flex items-center gap-4 px-4 py-2 border mt-8 mb-8 text-gray-600'>
           <p>{input}</p>
-          <img src={assets.cross_icon} alt="" className='cursor-pointer' onClick={()=>navigate("/course-list")} />
+          <img src={assets?.cross_icon} alt="" className='cursor-pointer' onClick={()=>navigate("/course-list")} />
         </div>
       }
-
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-16 gap-3 px-2 md:p-0'>
-      {filteredCourse.map((course, index) => <CourseCard key={index} course={course}/>)}
+      {items}
       </div>
     </div>
- 
     </>
   )
 }
