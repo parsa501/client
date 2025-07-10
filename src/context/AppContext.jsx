1f63494d8202e67a247b729ef2464248b2f0bc9a
parsa501 <safazadehparsa@gmail.com>
2025-07-09 23:46:30 -0500

    add MyEnrollments

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import humanizeDuration from "humanize-duration";
-import { useAuth, useUser } from "@clerk/clerk-react";
+
 
 export const AppContext = createContext();
 
@@ -11,9 +11,6 @@ export const AppContextProvider = (props) => {
   const currency = import.meta.env.VITE_CURRENCY;
   const navigate = useNavigate();
 
-  
-  const { getToken } = useAuth();
-  const { user } = useUser();
 
   const [allCourses, setAllCourses] = useState([]);
   const [isEducator, setIsEducator] = useState(true);
@@ -74,15 +71,7 @@ export const AppContextProvider = (props) => {
     fetchUserEnrolledCourse();
   }, []);
 
-  const logToken = async () => {
-    console.log(await getToken());
-  };
 
-  useEffect(() => {
-    if (user) {
-      logToken();
-    }
-  }, [user]);
 
   const value = {
     currency,
